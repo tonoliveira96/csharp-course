0d149422fe551da1793144ffe585d26a8700dbd7
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-11-30 21:27:00 -0300

    Seção 5: Construtores, palavra this, sobrecarga, encapsulamento

---
+++ section-5/ConsoleApp1/ConsoleApp1/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome { get; set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
