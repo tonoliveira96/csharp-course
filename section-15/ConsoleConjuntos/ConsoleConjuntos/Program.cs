63365192ab786fd96ebf9febb4bffdc402d3e8ea
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-12-19 20:59:45 -0300

    Seção 15: Conjuntos HAshSet e SortedSet

---
+++ section-15/ConsoleConjuntos/ConsoleConjuntos/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleConjuntos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int>() { 1, 5, 6, 7, 78, 2, 8 };
+            HashSet<int> b = new HashSet<int>() { 0, 1, 8, 14, 55 };
+
+            // UNION
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.Union(b);
+            PrintCollection(c);
+
+            // INTERCECTION
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            // DIFFERENCE
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(d);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
