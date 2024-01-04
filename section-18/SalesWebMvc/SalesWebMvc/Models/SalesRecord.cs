d9ae586a0583c1574f09e80d94cb9e97988b0ba1
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-01-03 23:28:26 -0300

    Seção 18: Outras entidades e segunda migration

---
+++ section-18/SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,26 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Saller { get; set; }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller saller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Saller = saller;
+        }
+
+        public SalesRecord() { }
+    }
+}
---
+++ section-18/SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Salles { get; set; } = new List<SalesRecord>();
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public Seller() { }
+
+        public void addSales(SalesRecord sr)
+        {
+            Salles.Add(sr);
+        }
+
+        public void remove(SalesRecord sr)
+        {
+            Salles.Remove(sr);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Salles.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
