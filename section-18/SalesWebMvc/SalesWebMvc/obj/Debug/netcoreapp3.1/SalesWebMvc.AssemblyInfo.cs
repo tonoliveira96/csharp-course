f70dfa696d1c455fc3a3a84ffa284319b5bd04cf
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-01-06 16:17:53 -0300

    Seção 18: Tratamento de exceção para delete

--- section-18/SalesWebMvc/SalesWebMvc/obj/Debug/netcoreapp3.1/SalesWebMvc.AssemblyInfo.cs
+++ section-18/SalesWebMvc/SalesWebMvc/obj/Debug/netcoreapp3.1/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae2b306ebb2b56a4b23ae5e3a1f9880f1140131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
