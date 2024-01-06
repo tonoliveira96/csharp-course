2ae2b306ebb2b56a4b23ae5e3a1f9880f1140131
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-01-06 15:26:43 -0300

    Seção 18: Validações

--- section-18/SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ section-18/SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -8,16 +8,28 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength =3, ErrorMessage = "name size should be between 3 an d 60.")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0,50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecords> Salles { get; set; } = new List<SalesRecords>();
